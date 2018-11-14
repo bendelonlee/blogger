0fe936843cac3042adcf8649c51d3941e9c1ec2d
Ben Lee <bendelonlee@gmail.com>
2018-11-14 15:29:52 -0700

    In article controller, adds before_action set_article to DRY out code. Adds update method.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,20 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to article_path(@article)
   end
 
   def create
@@ -19,13 +24,20 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy
     redirect_to articles_path
+  end
+
+  def edit
   end
 
   private
 
   def article_params
    params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
