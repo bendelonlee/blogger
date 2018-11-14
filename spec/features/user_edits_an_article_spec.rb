465fa907261e426dfaa42b855a23735b20cdf1fb
Ben Lee <bendelonlee@gmail.com>
2018-11-14 15:32:22 -0700

    Adds a further describe to user_edits_an_article_spec and it now clicks correctly on Update Article

--- spec/features/user_edits_an_article_spec.rb
+++ spec/features/user_edits_an_article_spec.rb
@@ -1,19 +1,21 @@
 require "rails_helper"
 describe "user edits an article" do
   describe "they link from the show page" do
-    it "diplays the article with the changes made" do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+    describe "they fill in and submit changes" do
+      it "displays the article with the changes made" do
+        article_1 = Article.create!(title: "Title 1", body: "Body 1")
 
-      visit article_path(article_1)
-      click_link "Edit"
+        visit article_path(article_1)
+        click_link "Edit"
 
-      #what sort of path should this expect?
-      fill_in "article[title]", with: "New Title!"
-      click_on "Save"
+        #what sort of path should this expect?
+        fill_in "article[title]", with: "New Title!"
+        click_on "Update Article"
 
-      expect(current_path).to eq(article_path(article_1))
-      expect(page).to have_content("New Title!")
-      expect(page).to_not have_content("Title 1")
+        expect(current_path).to eq(article_path(article_1))
+        expect(page).to have_content("New Title!")
+        expect(page).to_not have_content("Title 1")
+      end
     end
   end
 end
