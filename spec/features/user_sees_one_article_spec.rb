a4fdfb79bd1440f861990ce563b047eb14b6e1cf
Ben Lee <bendelonlee@gmail.com>
2018-11-15 10:52:25 -0700

    Adds expects for number of comments to be displayed

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -16,6 +16,7 @@ describe "user sees one article" do
       expect(page).to have_content(comment_1.body)
       expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
+      expect(page).to have_content("Comments (2)")
 
       click_link "<< Back to Articles"
 
@@ -37,6 +38,8 @@ describe "user sees one article" do
       expect(page).to have_content("Post a Comment")
       expect(page).to have_content("ME!")
       expect(page).to have_content("So many thoughts on this article.")
+      expect(page).to have_content("Comments (1)")
+
     end
   end
 end
