79c2101f751499e15dbd728404de4706b991fbae
Ben Lee <bendelonlee@gmail.com>
2018-11-15 10:09:58 -0700

    Adds lines to feature spec to test for comments

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -3,6 +3,8 @@ describe "user sees one article" do
   describe "they link from the article index" do
     it "displays information for one article" do
       article = Article.create!(title: "New", body: "How much")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comment")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
       visit '/articles'
 
@@ -10,6 +12,10 @@ describe "user sees one article" do
 
       expect(page).to have_content(article.title)
       expect(page).to have_content(article.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
 
       click_link "<< Back to Articles"
 
