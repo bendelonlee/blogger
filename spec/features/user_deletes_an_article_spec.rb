342027a3eaae46a13eb8e6aeabf2f74e30ce4c42
Ben Lee <bendelonlee@gmail.com>
2018-11-14 17:09:47 -0700

    Expects to not have title of deleted article after it revisits the articles index, this way it doesn't see the flash message.

--- spec/features/user_deletes_an_article_spec.rb
+++ spec/features/user_deletes_an_article_spec.rb
@@ -10,8 +10,9 @@ describe "user deletes an article" do
 
       expect(current_path).to eq(articles_path)
       expect(page).to have_content(article_2.title)
-      expect(page).to_not have_content(article_1.title)
       expect(page).to have_content("Article 'Title 1' was deleted.")
+      visit articles_path
+      expect(page).to_not have_content(article_1.title)
 
     end
   end
