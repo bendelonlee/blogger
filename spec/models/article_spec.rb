f85e24dcbc40e62ee8e7de13eb91cda26dc2388e
Ben Lee <bendelonlee@gmail.com>
2018-11-17 08:15:38 -0700

    Adds tests for article to have tags in feature spec and article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,4 +7,13 @@ describe Article, type: :model do
     it {should have_many(:comments)}
     it {should have_many(:taggings)}
   end
+  describe "instance methods" do
+    it "turns associated tags into a string" do
+      article = Article.create(title: "Mr. Peanutbutter", body: "Don't be allergic")
+      article.tags.create(name: "allergies")
+      article.tags.create(name: "peanuts")
+
+      expect(article.tag_list).to eq("allergies, peanuts")
+    end
+  end
 end
