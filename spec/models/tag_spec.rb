c586436ab900a849379a0042a7a427fffcab363b
Ben Lee <bendelonlee@gmail.com>
2018-11-17 08:41:36 -0700

    Tweaks feature spec for testing tags. Adds test for Tags to_s method

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -5,4 +5,10 @@ describe Tag, type: :model do
     it {should have_many(:taggings)}
     it {should have_many(:articles).through(:taggings)}
   end
+  describe "instance methods" do
+    it "should have a to_s method that returns the name of the tag" do
+      tag = Tag.create(name: "maytag")
+      expect(tag.to_s).to eq("maytag")
+    end
+  end
 end
