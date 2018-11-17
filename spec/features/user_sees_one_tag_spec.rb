3c7debdb8587e76c1387a158439aa8a195c27b3d
Ben Lee <bendelonlee@gmail.com>
2018-11-17 09:53:34 -0700

    Tweaks tag feature spec. All tests are now passing.

--- spec/features/user_sees_one_tag_spec.rb
+++ spec/features/user_sees_one_tag_spec.rb
@@ -2,16 +2,16 @@ require "rails_helper"
 describe "user sees one tag" do
   describe "they link from the article page" do
     it "should link to the tag and all its articles" do
-      article = Article.create!(title: "New Title", body: "New Body")
-      tag = article.tags.create!(name: "Name")
-      article = Article.create!(title: "Newer Title", body: "Newer Body")
-      tag = article.tags.create!(name: "Name")
+      article_1 = Article.create!(title: "New Title", body: "New Body")
+      tag = article_1.tags.create!(name: "name")
+      article_2 = Article.create!(title: "Newer Title", body: "Newer Body")
+      article_2.tags << tag
 
-      visit article_path(article)
-      click_link("Name")
+      visit article_path(article_1)
+      click_link("name")
 
-      expect(page).to have_content("Name")
-      # expect(page).to have_content("New Title")
+      expect(page).to have_content("name")
+      expect(page).to have_content("New Title")
       expect(page).to have_content("Newer Title")
     end
   end
